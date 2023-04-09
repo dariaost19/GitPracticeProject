0f5931c758cfe8902588673c33319c21a2c41aa9
dariaost19 <dariaost19@gmail.com>
2023-04-09 17:30:25 -0400

    chnged

--- src/test/java/PracticeTask/Task1Runner.java
+++ src/test/java/PracticeTask/Task1Runner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\Feature",glue = "TestCase1",dryRun = false,plugin = {"pretty"},tags = "@Test2")
+@CucumberOptions(features = "src\\test\\resources\\Feature",glue = "TestCase1",dryRun = false,plugin = {"pretty"})
 
 public class Task1Runner {
 
